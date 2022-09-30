311b6716d528caa0e0d7be83aca1f8848bdc8ad7
Amardeep soni <amardeep10as@gmail.com>
2022-09-30 23:24:03 +0545

    changed all html file to php

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Amardeep Chat App</title>
+    <title>Amardeep Chat App --> login</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
         integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -18,16 +18,6 @@
             <header>Amardeep Chat App</header>
             <form action="">
                 <div class="error-txt">This is an error message!</div>
-                <div class="name-details">
-                    <div class="field input">
-                        <label>First Name</label>
-                        <input type="text" placeholder="First Name">
-                    </div>
-                    <div class="field input">
-                        <label>Last Name</label>
-                        <input type="text" placeholder="Last Name">
-                    </div>
-                </div>
                 <div class="field input">
                     <label>Email Address</label>
                     <input type="text" placeholder="Enter your email">
@@ -37,15 +27,11 @@
                     <input type="password" placeholder="Enter your password">
                     <i class="fas fa-eye"></i>
                 </div>
-                <div class="field input image">
-                    <label>Select Image</label>
-                    <input type="file">
-                </div>
                 <div class="field input button">
                     <input type="submit" value="Continue to chat">
                 </div>
             </form>
-            <div class="link">Already signed up? <a href="login.html">Login now</a></div>
+            <div class="link">Not yet signed up? <a href="index.php">Signup now</a></div>
         </section>
     </div>
 
